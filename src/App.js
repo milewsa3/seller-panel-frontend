1cd13e4f04b2bd95931cf98e38138f34c6bc649f
Adrian <a.milewski@onet.eu>
2021-12-02 14:27:35 +0100

    Add i18n example

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
+import {Typography} from "@mui/material";
+import { useTranslation } from 'react-i18next';
+import LanguageSelector from './component/LanguageSelector';
+
 function App() {
+
+  const { t } = useTranslation();
+
   return (
     <div className="App">
-      Hello World
+      <Typography variant={"h1"}>{t("title")}</Typography>
+      <Typography variant={"body"}>{t("placeholder")}</Typography>
+      <LanguageSelector/>
     </div>
   );
 }
