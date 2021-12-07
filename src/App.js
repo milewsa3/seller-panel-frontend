e9a7ea2808db89a8111e72081f08861824fde0ad
Adrian <a.milewski@onet.eu>
2021-12-07 18:49:24 +0100

    Clear template

--- src/App.js
+++ src/App.js
@@ -1,20 +1,11 @@
-import {Typography} from "@mui/material";
 import { useTranslation } from 'react-i18next';
-import LanguageSelector from './component/LanguageSelector';
-import ThemeSwitcher from './component/ThemeSwitcher';
-import Counter from './component/Counter';
 
 function App() {
-
   const { t } = useTranslation();
 
   return (
     <div className="App">
-      <Typography variant={"h1"}>{t("title")}</Typography>
-      <Typography variant={"body"}>{t("placeholder")}</Typography>
-      <LanguageSelector/>
-      <ThemeSwitcher />
-      <Counter />
+
     </div>
   );
 }
--- src/app/store.js
+++ src/app/store.js
@@ -1,8 +1,6 @@
 import { configureStore } from '@reduxjs/toolkit'
-import counterReducer from '../slice/counterSlice'
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
   },
 })
