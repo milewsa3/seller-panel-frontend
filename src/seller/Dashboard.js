2dc54327feccacf9a7a145aba5a8693a849ddc4e
Adrian <a.milewski@onet.eu>
2022-01-05 23:28:04 +0100

    Dodanie gridu do Dashboard'u

--- src/seller/Dashboard.js
+++ src/seller/Dashboard.js
@@ -1,5 +1,5 @@
 import React, { useEffect } from 'react';
-import { Container, Typography } from '@mui/material';
+import { Grid, Paper } from '@mui/material';
 import { useNavigate } from 'react-router-dom';
 import { isUserLoggedIn } from '../util/user/userUtils';
 
@@ -13,11 +13,34 @@ const Dashboard = () => {
   }, [navigate]);
 
   return (
-    <Container>
-      <Typography variant="h2" color="text.primary">
-        Welcome home
-      </Typography>
-    </Container>
+    <Grid container sx={{ my: 8, height: '560px', width: '90%', mx: 'auto'}} spacing={3}>
+      <Grid item xs={5}>
+        <Paper sx={{height: '100%'}}>Orders</Paper>
+      </Grid>
+      <Grid item xs={7} container spacing={3} direction="column">
+        <Grid item xs={9} container spacing={3}>
+          <Grid item xs={3}>
+            <Paper sx={{height: '100%'}}>Buyer feedback</Paper>
+          </Grid>
+          <Grid item xs={9} container spacing={3} direction={"column"}>
+            <Grid item xs={8}>
+              <Paper sx={{height: '100%'}}>Ranking of offers</Paper>
+            </Grid>
+            <Grid item xs={4} container spacing={3}>
+              <Grid item xs={6}>
+                <Paper sx={{height: '100%'}}>Sales advice</Paper>
+              </Grid>
+              <Grid item xs={6}>
+                <Paper sx={{height: '100%'}}>Sales chart</Paper>
+              </Grid>
+            </Grid>
+          </Grid>
+        </Grid>
+        <Grid item xs={3}>
+          <Paper sx={{height: '100%'}}>Quality of sales</Paper>
+        </Grid>
+      </Grid>
+    </Grid>
   );
 };
 
